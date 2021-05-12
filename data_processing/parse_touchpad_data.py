17150b57b4d884cab2148f2bedc1800652ed8b61
Mark Menzynski <mmenzyns@redhat.com>
2021-05-12 17:53:39 +0200

    temp

--- data_processing/parse_touchpad_data.py
+++ data_processing/parse_touchpad_data.py
@@ -2,6 +2,7 @@ import pandas as pd
 import os
 from glob import glob
 import csv
+import numpy as np
 
 class TouchpadData:
 
@@ -10,42 +11,43 @@ class TouchpadData:
     size = None
     min_val = float('inf')
     max_val = float('-inf')
-    data = None
+    data = []
 
     def __init__(self, path=None):
         if not os.path.exists(path):
             raise Exception
         
+        if os.path.isdir(path):
+            for filename in glob(path + '/*'):
+                self.data.extend(self.load_file(filename))
+        elif os.path.isfile(path):
+            self.data = self.load_file(path)
+        else:
+            raise Exception('unexpected error')
+
+    def load_file(self, filename):
+        reader = csv.reader(open(filename, 'r'), delimiter='\t')
+
+        row_num = 0
         data = []
-        for filename in glob(path + '/*'):
-            reader = csv.reader(open(filename, 'r'), delimiter='\t')
+        for row in reader:
+            if row_num == 0:
+                new_width = int(row[1])
+                if self.width is None:
+                    self.width = new_width
+                else:
+                    assert(self.width == new_width)
+                new_height = int(row[3])
+                if self.height is None:
+                    self.height = new_height
+                else:
+                    assert(self.height == new_height)
 
-            row_num = 0
-            for row in reader:
-                if row_num == 0:
-                    new_width = int(row[1])
-                    if self.width is None:
-                        self.width = new_width
-                    else:
-                        assert(self.width == new_width)
-                    new_height = int(row[3])
-                    if self.height is None:
-                        self.height = new_height
-                    else:
-                        assert(self.height == new_height)
+            if row_num == 1:
+                data.extend(row)
 
-                if row_num == 1:
-                    data.extend(row)
-
-                if row_num == 2:
-                    new_min_val = int(row[1])
-                    if new_min_val < self.min_val:
-                        self.min_val = new_min_val
-                    
-                    new_max_val = int(row[3])
-                    if new_max_val > self.max_val:
-                        self.max_val = new_max_val
-
-                row_num += 1
-            self.size = self.height * self.width
-        self.data = pd.to_numeric(data, errors='ignore', downcast='integer').reshape(-1, self.height, self.width)
+            row_num += 1
+        self.size = self.height * self.width
+        return pd.to_numeric(data, errors='ignore', downcast='integer').reshape((-1, self.height, self.width))
+    
+original = TouchpadData('dataset/rotate/original')
